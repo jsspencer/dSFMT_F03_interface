8f6068fdfdb55097eb2dc65b720201e2e40389d8
James Spencer <james.s.spencer@gmail.com>
2017-11-10 11:50:55 +0000

    Integration check for dsfmt serialisation

--- test/testc.c
+++ test/testc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dSFMT.h"
 
 #define N 10000000
@@ -7,16 +8,29 @@ int main() {
 
     double sum;
     int i;
+    dsfmt_t dsfmt;
 
     sum = 0.0;
 
-    dsfmt_gv_init_gen_rand(7);
+    dsfmt_init_gen_rand(&dsfmt, 7);
 
     for (i=0; i<N; i++) {
-        sum += dsfmt_gv_genrand_close_open();
+        sum += dsfmt_genrand_close_open(&dsfmt);
     }
     
     printf("Sum of %i random numbers is %f\n", N, sum);
+
+    char* state = dsfmt_state_to_str(&dsfmt, NULL);
+    for (i=0; i<10; i++) {
+        printf("%i %f\n", i, dsfmt_genrand_close_open(&dsfmt));
+    }
+
+    dsfmt_str_to_state(&dsfmt, state, NULL);
+    for (i=0; i<10; i++) {
+        printf("%i %f\n", i, dsfmt_genrand_close_open(&dsfmt));
+    }
+
+    free(state);
 
     return 0;
 
--- test/testf.f90
+++ test/testf.f90
@@ -10,18 +10,33 @@ program test
 
     real(dp) :: rand_sum
     integer :: i
+    character(:), allocatable :: state_str
 
     call dSFMT_init(7, 50000, rng)
 
     rand_sum = 0.0_dp
-
     do i = 1, N
         rand_sum = rand_sum + get_rand_close_open(rng)
     end do
 
     write (6,'(a7,i8,a19,f14.6)') 'Sum of ', N, ' random numbers is ', rand_sum
 
+    call dsfmt_state_to_string(rng, state_str)
+
+    do i = 0, 9
+        write (6,'(i1,1x,f8.6)') i, get_rand_close_open(rng)
+    end do
+
     call dSFMT_end(rng)
 
+    call dSFMT_init(7, 50000, rng)
+    call dsfmt_str_to_state(rng, state_str)
+
+    do i = 0, 9
+        write (6,'(i1,1x,f8.6)') i, get_rand_close_open(rng)
+    end do
+
+    call dSFMT_end(rng)
+    deallocate(state_str)
 
 end program test
